79282309026bffc83b4d95f4a956f2db0ec60c53
A700472 <maria.filemonowicz@atos.net>
2019-08-20 14:47:56 +0200

    add codecov

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -3,14 +3,19 @@ const { Calculator } = require('../src/calc')
 const assert = require('assert')
 
 describe('Calculator', () => {
-    it('should add numbers', () => {
-        //assert.expectation(3)
-        assert.equal(Calculator.add(2, 2), 4)
-        // assert.equal(Calculator.add(2, 2), 5)
-        // assert.equal(Calculator.add(2, 2), 6)
+    for (var i = 0; i < 100; ++i) {
+        it('should add numbers', (done) => {
+            //assert.expectation(3)
+            assert.equal(Calculator.add(2, 2), 4)
+            // assert.equal(Calculator.add(2, 2), 5)
+            // assert.equal(Calculator.add(2, 2), 6)
 
-        // if (2 !== 3) {
-        //     throw new Error('2 is not equal to 3')
-        // }
-    })
+            // if (2 !== 3) {
+            //     throw new Error('2 is not equal to 3')
+            // }
+            setTimeout(() => {
+                done(), Math.random() * 300
+            })
+        })
+    }
 })
